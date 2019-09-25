044cfac6962a2bbc54be16936a3c3837101d46dc
Michael Passow <passowmichael@yahoo.com>
2019-09-24 20:10:04 -0400

    changed const to var in iconGenerator file

--- public/js/iconGenerator.js
+++ public/js/iconGenerator.js
@@ -1,7 +1,7 @@
 
 //Creates Random Icon for Search Button
-const hash = Math.floor(Math.random() * 1001);
+var hash = Math.floor(Math.random() * 1001);
 
-const icon = hashicon(String(hash), 50);
+var icon = hashicon(String(hash), 50);
 
 document.getElementById("icon").appendChild(icon);
